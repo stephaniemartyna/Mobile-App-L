fd27fc08fb0b772e36b6bd7d8026bbe9eac30194
stephaniemartyna <stephanie.martyna@edu.sait.ca>
2023-10-31 21:31:45 -0600

    Lab 3 Commit

--- App.jsx
+++ App.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 import ToDoForm from './ToDoForm';
 import ToDoList from './ToDoList';
 import { SafeAreaView } from 'react-native';
+import { useState } from 'react';
 
 
 function App() {
+  
+ // Use the useState hook to define a state variable to store the list of tasks.
+//Initialize the state with the following array of hard-coded tasks:
+//[
+//  'Do laundry',
+//  'Go to gym',
+//  'Walk dog'
+//]
+
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+  
   return (
     <SafeAreaView>
       <ToDoForm />
-      <ToDoList />
+      <ToDoList tasks={tasks} />
+
     </SafeAreaView>
   );
 }
